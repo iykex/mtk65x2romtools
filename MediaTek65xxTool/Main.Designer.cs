3e2250f70564358386d30c06e4e5902442ccec72
swoopae <senpaifuuzy@gmail.com>
2017-10-22 00:37:32 +0300

    0.2.0.0 - added custom chipset preset feature

--- MediaTek65xxTool/Main.Designer.cs
+++ MediaTek65xxTool/Main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.baseRomFile = new System.Windows.Forms.OpenFileDialog();
             this.portRomFile = new System.Windows.Forms.OpenFileDialog();
             this.portZipButton = new System.Windows.Forms.Button();
@@ -42,6 +43,9 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.mtButton4 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,9 +61,10 @@
             // 
             // portZipButton
             // 
-            this.portZipButton.Location = new System.Drawing.Point(95, 10);
+            this.portZipButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.portZipButton.Location = new System.Drawing.Point(158, 12);
             this.portZipButton.Name = "portZipButton";
-            this.portZipButton.Size = new System.Drawing.Size(176, 23);
+            this.portZipButton.Size = new System.Drawing.Size(241, 25);
             this.portZipButton.TabIndex = 0;
             this.portZipButton.Text = "New ROM Zip";
             this.portZipButton.UseVisualStyleBackColor = true;
@@ -67,9 +72,10 @@
             // 
             // stockZipButton
             // 
-            this.stockZipButton.Location = new System.Drawing.Point(95, 32);
+            this.stockZipButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.stockZipButton.Location = new System.Drawing.Point(158, 35);
             this.stockZipButton.Name = "stockZipButton";
-            this.stockZipButton.Size = new System.Drawing.Size(176, 23);
+            this.stockZipButton.Size = new System.Drawing.Size(241, 26);
             this.stockZipButton.TabIndex = 1;
             this.stockZipButton.Text = "Stock/Ported ROM Zip";
             this.stockZipButton.UseVisualStyleBackColor = true;
@@ -79,7 +85,7 @@
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Enabled = false;
-            this.radioButton1.Location = new System.Drawing.Point(3, 11);
+            this.radioButton1.Location = new System.Drawing.Point(12, 12);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(87, 17);
             this.radioButton1.TabIndex = 2;
@@ -90,7 +96,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(3, 32);
+            this.radioButton2.Location = new System.Drawing.Point(12, 35);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(86, 17);
             this.radioButton2.TabIndex = 3;
@@ -101,22 +107,25 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(3, 54);
+            this.radioButton3.Location = new System.Drawing.Point(12, 58);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(164, 17);
+            this.radioButton3.Size = new System.Drawing.Size(95, 17);
             this.radioButton3.TabIndex = 4;
-            this.radioButton3.Text = "Android 6.x-7.x (Experimental)";
+            this.radioButton3.Text = "Android 6.x-7.x";
             this.radioButton3.UseVisualStyleBackColor = true;
             this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(2, 176);
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(12, 234);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(269, 23);
+            this.button1.Size = new System.Drawing.Size(387, 23);
             this.button1.TabIndex = 5;
             this.button1.Text = "Do it!";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // mtButton1
@@ -154,12 +163,15 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.mtButton4);
             this.groupBox1.Controls.Add(this.mtButton2);
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.mtButton3);
             this.groupBox1.Controls.Add(this.mtButton1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 77);
+            this.groupBox1.Location = new System.Drawing.Point(12, 81);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(249, 93);
+            this.groupBox1.Size = new System.Drawing.Size(387, 147);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Chipset";
@@ -167,7 +179,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(155, 90);
+            this.label1.Location = new System.Drawing.Point(282, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 26);
             this.label1.TabIndex = 10;
@@ -175,18 +187,46 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(2, 204);
+            this.progressBar1.Location = new System.Drawing.Point(12, 284);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(268, 35);
+            this.progressBar1.Size = new System.Drawing.Size(387, 35);
             this.progressBar1.TabIndex = 11;
+            // 
+            // mtButton4
+            // 
+            this.mtButton4.AutoSize = true;
+            this.mtButton4.Location = new System.Drawing.Point(7, 95);
+            this.mtButton4.Name = "mtButton4";
+            this.mtButton4.Size = new System.Drawing.Size(143, 17);
+            this.mtButton4.TabIndex = 11;
+            this.mtButton4.Text = "Custom (name in textbox)";
+            this.mtButton4.UseVisualStyleBackColor = true;
+            this.mtButton4.CheckedChanged += new System.EventHandler(this.mtButton4_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(374, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Status: Idle";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(273, 251);
+            this.ClientSize = new System.Drawing.Size(409, 331);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.radioButton3);
@@ -194,9 +234,8 @@
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.stockZipButton);
             this.Controls.Add(this.portZipButton);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.MaximumSize = new System.Drawing.Size(289, 290);
-            this.MinimumSize = new System.Drawing.Size(289, 290);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.Text = "MediaTek 65xx ROM Tool";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -223,6 +262,9 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RadioButton mtButton4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- MediaTek65xxTool/Main.cs
+++ MediaTek65xxTool/Main.cs
@@ -25,6 +25,7 @@ namespace MediaTek65xxTool
 
         public string dirPath = Directory.GetCurrentDirectory();
         public string chipset = "0";
+        public string customChip = "";
 
         private int AndroidVer;
         private bool firstLaunch = true;
@@ -59,54 +60,64 @@ namespace MediaTek65xxTool
                 firstLaunch = false;
             }
 
-            if(firstLaunch)
+            if (firstLaunch)
             {
                 ini.Write("AndroidVer", "0", "Preferences");
                 ini.Write("Chipset", "0", "Preferences");
             } else
             {
                 AndroidVer = int.Parse(ini.Read("AndroidVer", "Preferences"));
-                chipset = int.Parse(ini.Read("Chipset", "Preferences")).ToString();
+                chipset = ini.Read("Chipset", "Preferences");
 
                 // i'm way too lazy to rewrite this as a switch even tho it would look so much prettier
 
-                if (AndroidVer == 1)
+                switch (AndroidVer)
                 {
-                    radioButton2.Checked = false;
-                    radioButton3.Checked = false;
-                    radioButton1.Checked = true;
-                } else if (AndroidVer == 2)
-                {
-                    radioButton1.Checked = false;
-                    radioButton3.Checked = false;
-                    radioButton2.Checked = true;
-                } else if (AndroidVer == 3)
-                {
-                    radioButton1.Checked = false;
-                    radioButton2.Checked = false;
-                    radioButton3.Checked = true;
-                } else
-                {
-                    radioButton1.Checked = false;
-                    radioButton2.Checked = false;
-                    radioButton3.Checked = false;
+                    case 1:
+                        radioButton2.Checked = false;
+                        radioButton3.Checked = false;
+                        radioButton1.Checked = true;
+                        break;
+                    case 2:
+                        radioButton1.Checked = false;
+                        radioButton3.Checked = false;
+                        radioButton2.Checked = true;
+                        break;
+                    case 3:
+                        radioButton1.Checked = false;
+                        radioButton2.Checked = false;
+                        radioButton3.Checked = true;
+                        break;
+                    default:
+                        radioButton1.Checked = false;
+                        radioButton2.Checked = false;
+                        radioButton3.Checked = false;
+                        break;
                 }
-
                 switch (chipset) {
                     case "6572":
                         mtButton1.Checked = true;
                         mtButton2.Checked = false;
                         mtButton3.Checked = false;
+                        mtButton4.Checked = false;
                         break;
                     case "6582":
                         mtButton1.Checked = false;
                         mtButton2.Checked = true;
                         mtButton3.Checked = false;
+                        mtButton4.Checked = false;
                         break;
                     case "6592":
                         mtButton1.Checked = false;
                         mtButton2.Checked = false;
                         mtButton3.Checked = true;
+                        mtButton4.Checked = false;
+                        break;
+                    case "custom":
+                        mtButton1.Checked = false;
+                        mtButton2.Checked = false;
+                        mtButton3.Checked = false;
+                        mtButton4.Checked = true;
                         break;
                     default:
                         mtButton1.Checked = false;
@@ -172,16 +183,21 @@ namespace MediaTek65xxTool
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (!File.Exists(dirPath + "/" + customChip + ".ini"))
+            {
+                MessageBox.Show("Please select a valid custom chipset preset. (Make sure what you typed is equal to what the .ini preset is named, for example you must type PRESET for the file PRESET.ini");
+                return;
+            }
             if (AndroidVer == 0)
             {
                 MessageBox.Show("Please select a valid android version for your ported rom. ");
-                Application.Exit();
+                return;
             }
 
-            if (chipset == "0")
+            if (chipset != "6572" && chipset != "6582" && chipset != "6592" && chipset != "custom")
             {
                 MessageBox.Show("Please select a valid chipset. ");
-                Application.Exit();
+                return;
             }
             else
             {
@@ -190,26 +206,185 @@ namespace MediaTek65xxTool
                 Directory.CreateDirectory(dirPath + "/temp/PortROM/");
                 Directory.CreateDirectory(dirPath + "/temp/FilesToPort/");
 
+                label2.Text = "Status: Extracting base ROM";
+
                 ZipActivity("extract", baseRomFile.FileName.ToString(), dirPath + "/temp/BaseROM");
                 progressBar1.Value = 20;
                 progressBar1.Refresh();
+
+                label2.Text = "Status: Extracting port ROM";
+
                 ZipActivity("extract", portRomFile.FileName.ToString(), dirPath + "/temp/PortROM");
                 progressBar1.Value = 40;
                 progressBar1.Refresh();
 
-                switch (AndroidVer)
+                if (chipset != "custom")
                 {
-                    case 1:
-                        KitkatPort();
-                        break;
-                    case 2:
-                        GeneralPort();
-                        break;
-                    case 3:
-                        GeneralPort();
-                        break;
+                    switch (AndroidVer)
+                    {
+                        case 1:
+                            KitkatPort();
+                            break;
+                        case 2:
+                            GeneralPort();
+                            break;
+                        case 3:
+                            GeneralPort();
+                            break;
+                    }
+                } else
+                {
+                    if (!File.Exists(dirPath + "/" + customChip + ".ini"))
+                    {
+                        MessageBox.Show("Please select a valid custom chipset preset. (Make sure what you typed is equal to what the .ini preset is named, for example you must type PRESET for the file PRESET.ini");
+                        return;
+                    } else
+                    {
+                        CustomPort();
+                    }
                 }
             }
+        }
+
+        void CustomPort()
+        {
+            IniParse preset = new IniParse(customChip + ".ini");
+
+            label2.Text = "Status: Checking preset consistency";
+
+            int dirsToMove = 0, filesToMove = 0, btFilesToMove = 0;
+
+            try
+            {
+                dirsToMove = int.Parse(preset.Read("Count", "Directories"));
+                filesToMove = int.Parse(preset.Read("Count", "Files"));
+                btFilesToMove = int.Parse(preset.Read("Count", "BootImg"));
+            } catch (Exception)
+            {
+                MessageBox.Show("Unexpected error.Error code: 0x05 (Corrupted preset file)");
+                Cleanup(false);
+            }
+            
+            label2.Text = "Status: Replacing drivers";
+
+            while (dirsToMove < 0) {
+                try
+                {
+                    Directory.Delete(dirPath + "/temp/PortROM" + preset.Read("Directory" + dirsToMove.ToString(), "Directories"), true);
+                    Directory.Move(dirPath + "/temp/BaseROM" + preset.Read("Directory" + dirsToMove.ToString(), "Directories"), dirPath + "/temp/PortROM" + preset.Read("Directory" + dirsToMove.ToString(), "Directories"));
+                } catch (Exception)
+                {
+                    MessageBox.Show("Unexpected error.Error code: 0x06 (Invalid directory path in preset)");
+                    Cleanup(true);
+                }
+                dirsToMove -= 1;
+            }
+
+            while (filesToMove < 0)
+            {
+                try
+                {
+                    File.Copy(dirPath + "/temp/BaseROM" + preset.Read("File" + filesToMove.ToString(), "Files"), dirPath + "/temp/PortROM" + preset.Read("File" + filesToMove.ToString(), "Files"), true);
+                } catch (Exception)
+                {
+                    MessageBox.Show("Unexpected error.Error code: 0x07 (Invalid file path in preset)");
+                    Cleanup(true);
+                }
+                filesToMove -= 1;
+            }
+
+            Directory.CreateDirectory(dirPath + "/temp/BaseROM/bootimage");
+            Directory.CreateDirectory(dirPath + "/temp/PortROM/bootimage");
+            ExtractEmbeddedResource(dirPath + "/temp/BaseROM/bootimage", "MediaTek65xxTool", bootimgtool);
+            ExtractEmbeddedResource(dirPath + "/temp/PortROM/bootimage", "MediaTek65xxTool", bootimgtool);
+
+            File.Move(dirPath + "/temp/BaseROM/boot.img", dirPath + "/temp/BaseROM/bootimage/boot.img");
+            File.Move(dirPath + "/temp/PortROM/boot.img", dirPath + "/temp/PortROM/bootimage/boot.img");
+            System.Threading.Thread.Sleep(5000);
+
+            label2.Text = "Status: Modifying boot image";
+
+            Process unpackStock = new Process();
+
+            unpackStock.StartInfo.FileName = dirPath + "/temp/BaseROM/bootimage/unpack.bat";
+            unpackStock.StartInfo.Arguments = "";
+            unpackStock.StartInfo.WorkingDirectory = dirPath + "/temp/BaseROM/bootimage/";
+            unpackStock.Start();
+            System.Threading.Thread.Sleep(1000);
+
+            Process unpackPort = new Process();
+
+            unpackPort.StartInfo.FileName = dirPath + "/temp/PortROM/bootimage/unpack.bat";
+            unpackPort.StartInfo.Arguments = "";
+            unpackPort.StartInfo.WorkingDirectory = dirPath + "/temp/PortROM/bootimage/";
+            unpackPort.Start();
+            System.Threading.Thread.Sleep(1000);
+
+            progressBar1.Value = 75;
+            progressBar1.Refresh();
+
+            System.Threading.Thread.Sleep(10000);
+
+            while (btFilesToMove != 0)
+            {
+                try
+                {
+                    File.Copy(dirPath + "/temp/BaseROM/bootimage" + preset.Read("File" + btFilesToMove.ToString(), "BootImg"), dirPath + "/temp/PortROM/bootimage" + preset.Read("File" + btFilesToMove.ToString(), "BootImg"), true);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Unexpected error.Error code: 0x08 (Invalid boot image files path in preset)");
+                    Cleanup(true);
+                }
+
+                btFilesToMove -= 1;
+            }
+
+            progressBar1.Value = 85;
+            progressBar1.Refresh();
+
+            unpackPort.StartInfo.FileName = dirPath + "/temp/PortROM/bootimage/repack.bat";
+            unpackPort.StartInfo.Arguments = "";
+            unpackPort.StartInfo.WorkingDirectory = dirPath + "/temp/PortROM/bootimage/";
+            unpackPort.Start();
+
+            System.Threading.Thread.Sleep(10000);
+
+            File.Copy(dirPath + "/temp/PortROM/bootimage/boot-new.img", dirPath + "/temp/PortROM/boot.img", true);
+
+            progressBar1.Value = 90;
+            progressBar1.Refresh();
+            
+            Directory.Delete(dirPath + "/temp/PortROM/bootimage/", true);
+
+            label2.Text = "Status: Repacking ported ROM";
+
+            ZipActivity("repack", dirPath + "/temp/PortROM", dirPath + "/portedROM.zip");
+
+            label2.Text = "Status: Cleaning up";
+
+            progressBar1.Value = 95;
+            progressBar1.Refresh();
+
+            DirectoryInfo dx = new DirectoryInfo(dirPath + "/temp/");
+
+            foreach (FileInfo file in dx.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in dx.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+
+            progressBar1.Value = 100;
+            progressBar1.Refresh();
+
+            Directory.Delete(dirPath + "/temp/");
+
+            label2.Text = "Status: Idle";
+
+            MessageBox.Show("Job done! Please consider re-checking the updater-script and modifying it for your device/Doing some build.prop tweaks before flashing! :)");
         }
 
         void KitkatPort()
@@ -222,6 +397,8 @@ namespace MediaTek65xxTool
         }
         void GeneralPort()
         {
+            label2.Text = "Status: Replacing drivers";
+
             Directory.Delete(dirPath + "/temp/PortROM/system/etc/firmware/", true);
             Directory.Delete(dirPath + "/temp/PortROM/system/etc/wifi/", true);
             Directory.Delete(dirPath + "/temp/PortROM/system/etc/bluetooth/", true);
@@ -274,6 +451,8 @@ namespace MediaTek65xxTool
             System.Threading.Thread.Sleep(5000);
 
             // Boot image unpacking-repacking start. 
+
+            label2.Text = "Status: Modifying boot image";
 
             Process unpackStock = new Process();
 
@@ -337,8 +516,12 @@ namespace MediaTek65xxTool
 
             Directory.Delete(dirPath + "/temp/PortROM/bootimage/", true);
 
+            label2.Text = "Status: Repacking ported ROM";
+
             ZipActivity("repack", dirPath + "/temp/PortROM", dirPath + "/portedROM.zip");
-            
+
+            label2.Text = "Status: Cleaning up";
+
             progressBar1.Value = 95;
             progressBar1.Refresh();
 
@@ -357,6 +540,8 @@ namespace MediaTek65xxTool
             progressBar1.Refresh();
 
             Directory.Delete(dirPath + "/temp/");
+
+            label2.Text = "Status: Idle";
 
             MessageBox.Show("Job done! Please consider re-checking the updater-script and modifying it for your device/Doing some build.prop tweaks before flashing! :)");
         }
@@ -377,6 +562,36 @@ namespace MediaTek65xxTool
         {
             chipset = "6592";
             ini.Write("Chipset", chipset, "Preferences");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            customChip = textBox1.Text;
+        }
+
+        private void mtButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            chipset = "custom";
+            ini.Write("Chipset", chipset, "Preferences");
+        }
+
+        void Cleanup(bool exit)
+        {
+            DirectoryInfo direcx = new DirectoryInfo(dirPath + "/temp/");
+
+            foreach (FileInfo file in direcx.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in direcx.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+
+            Directory.Delete(dirPath + "/temp/");
+
+            if (exit)
+                Application.Exit();
         }
     }
 }
